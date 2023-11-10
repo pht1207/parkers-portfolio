764481608b421cacbd77ea5aeaabcd8f648f21b1
pht1207 <parkerthroneberry@gmail.com>
2023-11-10 11:53:57 -0600

    Added more detail to the description of 3d printing service

--- src/Pages/Projects/ProjectsPage.js
+++ src/Pages/Projects/ProjectsPage.js
@@ -12,9 +12,11 @@ function Projects() {
                 title="3D Printing Application"
                 URL="print.parkert.dev"
                 description1="This is a 3D Printing application developed using React for the front end and Node.js for the backend. This is how it works:" 
-                description2="File Input: Users input their stl files, select a print quality for them, then the files are sent to the backend to be parsed." 
-                description3="The backend parses their stl file to gcode and presents the 3D gcode object to them to be viewed."
-                description4="The user can then add the gcode order to their cart and purchase it via Stripe.js."
+
+                description2="File Input: Users input their stl files and select a desired print quality. The file is then sent to the backend." 
+                description3="Backend Processing: The backend parses their stl file to gcode and presents the 3D gcode object to them to be viewed."
+                description4="3D Preview: The user can then add the gcode order to their cart and purchase it via Stripe.js."
+                description5="Order and Payment: The user can then add the gcode order to their cart and purchase it via Stripe.js."
                 imgSrc="../../images/ProjectsImages/PrintAppScreenshot1.png"
                 />
 
@@ -22,6 +24,7 @@ function Projects() {
                 title="Transcoding Application"
                 URL="trasncoder.parkert.dev"
                 description1="1" 
+
                 description2="2" 
                 description3="3"
                 description4="4"
@@ -31,6 +34,7 @@ function Projects() {
                 title="Home Page Application"
                 URL="home.parkert.dev"
                 description1="1" 
+
                 description2="2" 
                 description3="3"
                 description4="4"
@@ -40,6 +44,7 @@ function Projects() {
                 title="Transcoding Application"
                 URL="print.parkert.dev"
                 description1="1" 
+
                 description2="2" 
                 description3="3"
                 description4="4"
