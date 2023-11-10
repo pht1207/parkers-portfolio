1fadb1cb97087b86a8a04193b1ff0ccb99bd1c2a
pht1207 <parkerthroneberry@gmail.com>
2023-11-09 21:32:14 -0600

    Added basis for the projectcomponents inside of the projectspage as well as added a photo for the print-app project

--- src/Pages/Projects/ProjectComponent.js
+++ src/Pages/Projects/ProjectComponent.js
@@ -1,11 +1,13 @@
-import './Project.css'
+import './ProjectComponent.css'
 import { useEffect, useState } from 'react';
+import '../../images/ProjectsImages/PrintAppScreenshot1.png'
 
-function ProjectComponent() {
+
+function ProjectComponent(props) {
 
     return (
         <div className='ProjectComponent'>
-            <h1>These are my projects</h1>
+            <h1>{props.title}</h1>
             <p>asdasfs</p>
         </div>
     );
--- src/Pages/Projects/ProjectsPage.js
+++ src/Pages/Projects/ProjectsPage.js
@@ -1,11 +1,23 @@
-import './Projects.css'
+import ProjectComponent from './ProjectComponent';
+import './ProjectsPage.css'
 import { useEffect, useState } from 'react';
+import '../../images/ProjectsImages/PrintAppScreenshot1.png'
 
 function Projects() {
 
     return (
         <div className='ProjectsDiv'>
             <h1>These are my projects</h1>
+            <ul>
+                <ProjectComponent 
+                title="3D Printing Application"
+                websiteName="print.parkert.dev"
+                descriptor1="" 
+                descriptor2="" 
+                descriptor3=""
+                descriptor4=""
+                imgSrc="../../"/>
+            </ul>
             <p>asdasfs</p>
         </div>
     );
