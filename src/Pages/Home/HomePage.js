36bc6db5ddcad0b724c239beba5bd094b9f3783c
pht1207 <parkerthroneberry@gmail.com>
2023-11-10 21:34:08 -0600

    Changed some minor css

--- src/Pages/Home/HomePage.js
+++ src/Pages/Home/HomePage.js
@@ -6,16 +6,15 @@ import ContactInfo from './ContactInfo'
 function Home() {
   return (
     <div className="Home">
-
         <div className='Opening'>
-          <HiMyNameIs speed={100} html={
-          <>
-          </>
-          }
-          />
-          <ContactInfo/>
+            <HiMyNameIs speed={100} html={
+            <>
+            </>
+            }
+            />
+            <ContactInfo/>
         </div>
-
+        <div className='InvisibleDiv'/>
     </div>
   );
 }
