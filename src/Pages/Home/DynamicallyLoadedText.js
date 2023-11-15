db7265228ee9afc22cb8f2b9c9227c88ecd946e6
pht1207 <parkerthroneberry@gmail.com>
2023-11-14 22:15:45 -0600

    Better method made for displaying the text

--- src/Pages/Home/DynamicallyLoadedText.js
+++ src/Pages/Home/DynamicallyLoadedText.js
@@ -2,23 +2,31 @@ import {React, useEffect, useState} from "react";
 
 
 function DynamicallyLoadedText(props) {
-    const propText = props.inputText;
-    const [dynamicText, setDynamicText] = useState("");
-
+    //const propText = props.inputText;
+    const [displayedText, setDisplayedText] = useState("a");
     //Runs when the component is first loaded
-    let i = 0;
-    useEffect(() =>{
-      if(i <= propText.length){
-        const timer = setTimeout(() => {
-            setDynamicText(dynamicText+(propText.charAt(i)));
-            i++;
-        }, 1000)
+    useEffect(() => {
+      delayedForLoop();
+    }, []);
+
+
+
+    async function delayedForLoop(){
+        for(let i = 0; i < 5; i++){
+          await delay(1000);
+          await setDisplayedText(displayedText => displayedText + "a")
+          console.log("a");
       }
-    },[])
+    }
+
+      function delay(ms){
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+
 
   return (
     <div className="Test">
-        <p>{dynamicText}</p>
+        <p>{displayedText}</p>
     </div>
   );
 }
