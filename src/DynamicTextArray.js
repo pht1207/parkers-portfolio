33880d198e445c74be7039472198bd183f5454e0
pht1207 <parkerthroneberry@gmail.com>
2023-11-15 23:44:32 -0600

    DynamicTextArray will print multiple elements but not at different times

--- src/DynamicTextArray.js
+++ src/DynamicTextArray.js
@@ -1,56 +1,35 @@
 import React, { useEffect, useState } from 'react';
 
-function DynamicTextArray(props) {
+function TypingText({ text, type }) {
   const [displayedText, setDisplayedText] = useState('');
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const [dynamicElementArray, setDynamicElementArray] = useState([]);
-
-
-  const propText = props.elementArray[0].props.children;
-  const type = props.elementArray[0].type;
-  const className = props.elementArray[0].props.className;
-
-
-//Constructor for creating user objects for the users[] array.
-function dynamicElementObject(propText, type, className){
-  this.propText = propText;
-  this.type = type;
-  this.className = className;
-}
-
-  //Turns all the props into objects on render so they can be accessed
-  let propDataArray = [];
   useEffect(() => {
-    for(let i = 0; i < props.elementArray.length; i++){
-      propDataArray.push(dynamicElementObject(
-        props.elementArray[i].props.children,
-        props.elementArray[i].type,
-        props.elementArray[i].props.className
-        ))
-    }
-  },[])
-
-
-
-  useEffect(() => {
-    // Check if we still have characters left to add
-    if (currentIndex < propText.length) {
+    if (currentIndex < text.length) {
       const timer = setTimeout(() => {
-        // Add the next character to the displayed text
-        setDisplayedText(displayedText + propText.charAt(currentIndex));
-        // Move to the next character
+        setDisplayedText(currentText => currentText + text.charAt(currentIndex));
         setCurrentIndex(currentIndex + 1);
-      }, 50); // Adjust the timing for typing speed
+      }, 300); // Adjust the timing for typing speed
 
-      // Cleanup function to clear the timer
       return () => clearTimeout(timer);
     }
-  }, [currentIndex, propText, displayedText]);
+  }, [currentIndex, text]);
 
-  const dynamicComponent = React.createElement(type, null, displayedText);
-
-  return <div className={className}>{dynamicComponent}</div>;
+  return React.createElement(type, null, displayedText);
 }
 
-export default DynamicTextArray;
+function DynamicallyLoadedText({ elements }) {
+  return (
+    <div>
+      {elements.map((element, index) => (
+        <TypingText
+          key={index}
+          text={element.props.children}
+          type={element.type}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default DynamicallyLoadedText;
