75813f6eb6441499db7fe1cc3e56e77ce2fd05be
pht1207 <parkerthroneberry@gmail.com>
2023-11-16 13:58:43 -0600

    Styling added to the HiMyNameIs component

--- src/Pages/Home/HiMyNameIs.js
+++ src/Pages/Home/HiMyNameIs.js
@@ -8,7 +8,7 @@ function HiMyNameIs(props) {
 
     const dynamicElementText = ("Hi, my name is, \nParker Throneberry. \n\nI like to make things using JavaScript :) \n\nI am a full stack web dev who specializes in engineering web platforms. \n\nI graduate from UNTD in Spring of 2024 with a bachelors in Information Technology!");
 
-    const dynamicElement = <p className='test'>{dynamicElementText}</p>
+    const dynamicElement = <p className='OpeningText'>{dynamicElementText}</p>
 
 
     return (
